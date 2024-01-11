fdca68d150e30c6100dbadf0d9feee5ab85d91e0
makeit80 <hidlruddnr98@gmail.com>
2024-01-11 13:07:12 +0900

    Layout Design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Home = () => {
   return (
-    <div>
-        Home
-        create dev
-    </div>
+    <StTestImg src={process.env.PUBLIC_URL + '/images/testImg.jpg'} />
   )
 }
+
+const StTestImg = styled.img`
+  width: 100vw;
+  height: 100vh;  
+  background-size: cover;
+  object-fit: cover;
+`
 
 export default Home
